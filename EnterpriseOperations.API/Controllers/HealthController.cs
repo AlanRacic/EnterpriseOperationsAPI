fc5e5afc56f7dc004122ba74713c11b730c073ef
Alan Račić <alan.racic@gmail.com>
2026-05-21 12:37:36 +0200

    Add project files.

---
+++ EnterpriseOperations.API/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseOperations.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok(new 
+            {
+                Status = "Healthy",
+                Application = "Enterprise Operations API",
+                Timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
