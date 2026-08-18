15aa1dbcf8391cf9697c259c20c9d27d4c379044
Alan Račić <alan.racic@gmail.com>
2026-08-18 13:30:20 +0200

    Configure early test host connection string for SQL Testcontainers

--- tests/EnterpriseOperations.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
+++ tests/EnterpriseOperations.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace EnterpriseOperations.IntegrationTests.Infrastructure;
 
@@ -12,6 +13,21 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         _connectionString = connectionString;
     }
 
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(configurationBuilder =>
+        {
+            var hostConfiguration = new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:DefaultConnection"] = _connectionString
+            };
+
+            configurationBuilder.AddInMemoryCollection(hostConfiguration);
+        });
+
+        return base.CreateHost(builder);
+    }
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
@@ -20,7 +36,6 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         {
             var testConfiguration = new Dictionary<string, string?>
             {
-                ["ConnectionStrings:DefaultConnection"] = _connectionString,
                 ["Cache:Provider"] = "Memory",
                 ["BackgroundJobs:Enabled"] = "false",
                 ["Database:ApplyMigrationsOnStartup"] = "true",
