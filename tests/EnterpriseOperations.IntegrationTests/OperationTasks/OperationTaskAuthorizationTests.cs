86927c95e06406293ad6eebd360be580e8df4f26
Alan Račić <alan.racic@gmail.com>
2026-08-19 10:05:03 +0200

    Add authorization integration test for OperationTasks endpoint

---
+++ tests/EnterpriseOperations.IntegrationTests/OperationTasks/OperationTaskAuthorizationTests.cs
@@ -0,0 +1,33 @@
+﻿using EnterpriseOperations.IntegrationTests.Infrastructure;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnterpriseOperations.IntegrationTests.OperationTasks;
+
+[Collection(IntegrationTestCollection.Name)]
+public class OperationTaskAuthorizationTests
+{
+    private readonly MsSqlContainerFixture _sqlFixture;
+
+    public OperationTaskAuthorizationTests(MsSqlContainerFixture sqlFixture)
+    {
+        _sqlFixture = sqlFixture;
+    }
+
+    [Fact]
+    public async Task GetOperationTasks_WithoutAuthentication_ReturnsUnauthorized() 
+    {
+        // Arrange
+        await using var factory = new CustomWebApplicationFactory(_sqlFixture.ConnectionString);
+
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/OperationTasks");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
